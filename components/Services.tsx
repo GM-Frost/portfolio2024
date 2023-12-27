7ef52edc5fd242cd6a69a3e3cd22c42a8a0d375f
Frosty <nayanbastola777@gmail.com>
2023-12-27 18:07:59 -0500

    Service Section Complete

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,9 +1,65 @@
-import React from "react";
+import { GanttChartSquare, Blocks, Gem } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-type Props = {};
-
-const Services = (props: Props) => {
-  return <div>Services</div>;
+const servicesData = [
+  {
+    icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
+    title: "UX Design",
+    description:
+      "I help our clients create brands, build digital products and services, innovate, find opportunities and validate ideas",
+  },
+  {
+    icon: <Blocks size={72} strokeWidth={0.8} />,
+    title: "UI Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam!",
+  },
+  {
+    icon: <Gem size={72} strokeWidth={0.8} />,
+    title: "App Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam!",
+  },
+];
+const Services = () => {
+  return (
+    <section className="mb-12 xl:mb-36">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">
+          My Services
+        </h2>
+        {/* ITEMS */}
+        <div className="grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24 xl:gap-x-8">
+          {servicesData.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+              >
+                <CardHeader className="text-primary absolute -top-[60px]">
+                  <div className="w-[140px] h-[80px] bg-white dark:bg-background rounded-full flex justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className="text-center">
+                  <CardTitle className="mb-4">{item.title}</CardTitle>
+                  <CardDescription className="text-lg">
+                    {item.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
